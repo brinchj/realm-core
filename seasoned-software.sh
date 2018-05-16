df05d18755136db382cd8cffa26bcb8032ef3585
Johan Brinch <brinchj@gmail.com>
2018-05-16 15:50:47 +0200

    Add seasoned software library config and update build script

--- seasoned-software.sh
+++ seasoned-software.sh
@@ -9,4 +9,4 @@ cd build
 
 cmake -D CMAKE_BUILD_TYPE=Debug -D REALM_ENABLE_ASSERTIONS=ON -D REALM_LIBFUZZER=ON -D REALM_MAX_BPNODE_SIZE=1000 -D REALM_ENABLE_ENCRYPTION=ON ..
 make -j4 realm-libfuzzer
-register-binary fuzzgroup test/fuzzy/realm-libfuzzer
+upload-binary test/fuzzy/realm-libfuzzer
